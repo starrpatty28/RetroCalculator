fcc5b3030ffa2e285210a92b1e0592cb1fec8ac9
starrpatty28 <starrpatty28@gmail.com>
2017-03-28 15:47:46 -0700

    add IBAction for all operators buttons, add sound for all operators, add different functions to run code, add images for launch screen

--- RetroCalculator/RetroCalculator/ViewController.swift
+++ RetroCalculator/RetroCalculator/ViewController.swift
@@ -15,16 +15,21 @@ class ViewController: UIViewController {
     
     var btnSound: AVAudioPlayer!
     
-    enum Operation: String {
+    enum Operation: String {   //All the operator cases with the calculator
         case Divide = "/"
         case Multiply = "*"
-        case Subtract = "_"
-        case Addition = "+"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
         
         
     }
-    
+    var currentOperation = Operation.Empty   //when you open your calculator your operation is empty
     var runningNumber = ""  //iniialize as a empty string
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    
     
 
     override func viewDidLoad() {
@@ -40,6 +45,8 @@ class ViewController: UIViewController {
             
         }
         
+        outputLbl.text = "0"
+        
     }
     
     @IBAction func numberPressed(sender: UIButton) {
@@ -50,6 +57,28 @@ class ViewController: UIViewController {
         
     }
     
+    //create IBActions for all the operators
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
+    }
+    
+ 
     func playSound() {
         if btnSound.isPlaying {
             btnSound.stop()
@@ -58,5 +87,38 @@ class ViewController: UIViewController {
         btnSound.play()
     }
     
+    func processOperation(operation: Operation) {     //this fnction process the operations
+        playSound()
+        if currentOperation != Operation.Empty {
+            
+            //A user selected an operator, but then selected another operator without first enetering a number
+            if runningNumber != "" {   //if running number value is not empty
+               rightValStr = runningNumber
+               runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {                   //when operators are pressed, we will have an action
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLbl.text = result   //after calculation is result
+            }
+            
+            currentOperation = operation
+        } else {
+            //This is the first time an operator has been pressed
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
 }
+
+
 
